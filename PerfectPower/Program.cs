5d50c02567a6d471ffc72f45b93981e72343f6ba
Vadim <bey1705@gmail.com>
2020-04-16 10:42:45 +0300

    This program has method which called 'PerfectPower', takes an positive integer and checks if this value is a perfect power.

---
+++ PerfectPower/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfectPower
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter the number in digits and press Enter: ");
+			int num = int.Parse(Console.ReadLine());
+
+			var sqrtResult = Math.Sqrt(num);
+
+			if (sqrtResult % 1 != 0)
+				Console.WriteLine("null");
+			else
+			{
+				int[] res = PerfectPower(num);
+
+				Console.WriteLine($"Your number is {res[0]}, power is {res[1]}");
+			}
+
+			Console.ReadLine();
+		}
+
+		static int[] PerfectPower(int n)
+		{
+			for (double i = 2; i <= Math.Sqrt(n); i++)
+			{
+				double power = 2;
+				double number = Math.Pow(i, power);
+
+				while (number < n && number > 0)
+				{
+					power++;
+					number = Math.Pow(i, power);
+				}
+
+				if (number == n)
+				{
+					return new int[]
+					{
+						(int)i, (int)power
+					};
+				}
+			}
+
+			return null;
+		}
+	}
+}
